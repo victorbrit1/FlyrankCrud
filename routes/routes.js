925d1319e3b0e7e9692621384afec65fe64c5dd2
Victor <rotcivbrito2003@gmal.com>
2026-07-31 14:35:26 -0300

    stage 3: create with validation

--- routes/routes.js
+++ routes/routes.js
@@ -42,4 +42,18 @@ router.get("/tasks/:id", (req,res) =>{
     return res.status(404).json({error:`task ${paramsId} not found`})        
 })
 
+router.post("/tasks", (req,res) =>{
+    const addedTask = {}
+
+    addedTask.title = req.body.title
+    if(!req.body.title)
+        return res.status(400).json({error:"title is required" })
+    addedTask.done = false
+    greaterId = tasks.length ? Math.max(...tasks.map(tasks => tasks.id)): 0
+    addedTask.id = greaterId + 1
+    tasks.push(addedTask)
+    return res.status(201).json(addedTask) 
+}
+)
+
 module.exports = router

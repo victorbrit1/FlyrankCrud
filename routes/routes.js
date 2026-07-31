ee29b3c979ce02f7fbb5db68375bcbd06c42fdbc
Victor <rotcivbrito2003@gmal.com>
2026-07-31 16:01:39 -0300

    stage 4: Update e Delete

--- routes/routes.js
+++ routes/routes.js
@@ -33,8 +33,8 @@ router.get("/tasks", (req,res) =>{
     res.status(200).json(tasks)
 })
 router.get("/tasks/:id", (req,res) =>{
-    paramsId = Number(req.params.id)
-    for(task of tasks){
+    const paramsId = Number(req.params.id)
+    for(const task of tasks){
         if(task.id === paramsId){
             return res.status(200).json(task)
         }
@@ -53,7 +53,40 @@ router.post("/tasks", (req,res) =>{
     addedTask.id = greaterId + 1
     tasks.push(addedTask)
     return res.status(201).json(addedTask) 
-}
-)
+})
+
+router.put("/tasks/:id", (req,res) =>{
+    const paramsId = Number(req.params.id)
+    for(const task of tasks){
+        if(task.id === paramsId){
+            if(req.body.title === undefined && req.body.done === undefined){
+                return res.status(400).send("title or done required")
+            }
+            if(req.body.title !== undefined){
+                if(typeof req.body.title !== "string")
+                    return res.status(400).json({error:"title must be string"})
+                task.title = req.body.title
+            }
+            if(req.body.done !== undefined){
+                if(typeof req.body.done !== "boolean")
+                    return res.status(400).json({error:"done must be boolean"})
+                task.done = req.body.done
+            }
+            return res.status(200).json(task)
+        }
+    }
+    return res.status(404).json({message: `Task ${paramsId} not found`})
+})
+
+router.delete("/tasks/:id", (req,res) =>{
+    const paramsId = Number(req.params.id)
+    
+    const index = tasks.findIndex(task => task.id === paramsId)
+    if(index === -1)
+        return res.status(404).json({message:"task not found"})
+
+    tasks.splice(index,1)
+    return res.status(204).send()
+})
 
 module.exports = router

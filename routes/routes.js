49c02208cfc00a7fbbbcd5a33494e1fe84cf2df1
Victor <rotcivbrito2003@gmal.com>
2026-07-31 12:11:33 -0300

    stage 2: read endpoints with 404

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const router = express.Router()
 
+const tasks = [
+    {
+        id: 1,
+        title: "Study Express",
+        done: false
+    },
+    {
+        id: 2,
+        title: "Build Task API",
+        done: true
+    },
+    {
+        id: 3,
+        title: "Test endpoints",
+        done: false
+    }
+]
+
 router.get("/health", (req,res) =>{
     res.status(200).json({status: "ok"})
 })
@@ -10,6 +28,18 @@ router.get("/", (req,res) =>{
         "version": "1.0",
         "endpoints": ["/tasks"]
     })
+})
+router.get("/tasks", (req,res) =>{
+    res.status(200).json(tasks)
+})
+router.get("/tasks/:id", (req,res) =>{
+    paramsId = Number(req.params.id)
+    for(task of tasks){
+        if(task.id === paramsId){
+            return res.status(200).json(task)
+        }
+    }
+    return res.status(404).json({error:`task ${paramsId} not found`})        
 })
 
 module.exports = router

ea9747503949b6d3d03ee50650a663fdbbd7d458
Victor <rotcivbrito2003@gmal.com>
2026-08-03 10:13:28 -0300

    stage 5 Swagger UI

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = require("./routes/routes")
+const swaggerUI = require("swagger-ui-express")
+const openapi = require("./docs/openapi.json")
 const app = express()
 
 const PORT = 3000
 
 app.use(express.json())
 app.use(router)
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(openapi))
 
 app.listen(PORT, () =>{
     console.log("Server running")
